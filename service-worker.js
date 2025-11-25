86b705a46f8082f5404c7bcb8d00d36737c92953
AngeloRamirez1983 <aramirezsanmartin@gmail.com>
2025-11-25 19:17:23 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,9 @@
-const CACHE_NAME = "semillitas-ventas-v1";
+const CACHE_NAME = "semillitas-ventas-v2";
 
 const URLS_TO_CACHE = [
-  "index.html",
-  "manifest.json",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
   "img/semillitas.jpg",
   "img/empanada.jpg",
   "img/papas.jpg",
@@ -10,34 +11,32 @@ const URLS_TO_CACHE = [
   "img/icon-512.png"
 ];
 
-// Instalar: cachea archivos básicos
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
   );
 });
 
-// Activar: limpiar caches viejos si se cambia la versión
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
+    caches.keys().then((keys) =>
       Promise.all(
         keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
       )
     )
   );
 });
 
-// Fetch: responde desde cache primero, luego red
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(resp => {
+    caches.match(event.request).then((cachedResponse) => {
       return (
-        resp ||
+        cachedResponse ||
         fetch(event.request).catch(() => {
-          // Aquí podrías devolver algo por defecto si lo deseas
+          // Si quieres, aquí podrías devolver una página offline.
+          return cachedResponse;
         })
       );
     })
